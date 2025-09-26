7a950798a0bfcd43880732e72883c3368bbf6c69
Aman sah <amanrck96@gmail.com>
2025-09-26 06:55:03 +0000

    createConsoleError@https://9000-firebase-studio-1758867283952.cluster-y7

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -42,11 +42,10 @@ export default function Contact() {
             </div>
             <div className="aspect-video w-full bg-muted rounded-lg overflow-hidden">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.838521799983!2d77.62227707481028!3d12.9822097873323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae168ce518933d%3A0x26c2e399033cb244!2sD&#39;Souza%20Layout%2C%20Ashok%20Nagar%2C%20Bengaluru%2C%20Karnataka%20560001%2C%20India!5e0!3m2!1sen!2sus!4v1758957813009!5m2!1sen!2sus"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.838521799983!2d77.62227707481028!3d12.9822097873323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae168ce518933d%3A0x26c2e399033cb244!2sD'Souza%20Layout%2C%20Ashok%20Nagar%2C%20Bengaluru%2C%20Karnataka%20560001%2C%20India!5e0!3m2!1sen!2sus!4v1758957813009!5m2!1sen!2sus"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
-                allowFullScreen={false}
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
